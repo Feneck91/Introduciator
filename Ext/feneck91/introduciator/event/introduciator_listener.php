b225a30bba937007a3ee1aab2f31af85e68ef09d
Stéphane CHÂTEAU <schateau@laposte.net>
2019-01-17 03:06:52 +0000

    Add most of functionalities. Missing GUI information in user profile.

--- Ext/feneck91/introduciator/event/introduciator_listener.php
+++ Ext/feneck91/introduciator/event/introduciator_listener.php
@@ -55,14 +55,38 @@ class introduciator_listener implements \Symfony\Component\EventDispatcher\Event
 	/**
 	 * Called by framework to get event list.
 	 *
+	 * See : https://wiki.phpbb.com/Event_List
+	 *
 	 * @return An array that contains event list with associated callback for each event.
 	 */
 	static public function getSubscribedEvents()
 	{
 		return array(
-			'core.viewtopic_modify_page_title'          => array('on_before_quickreply_displayed', -2),	// Hide quick reply if user is not allowed to post
-			'core.posting_modify_template_vars'			=> 'on_displaying_posting_screen',				// When user post a message, verify the introduce has been done
-			'core.posting_modify_submit_post_before'	=> 'on_submit_post',							// When user post a message, verify the introduce has been done
+			'core.viewtopic_modify_page_title'							=> array('on_before_quickreply_displayed', -2),	// Hide quick reply if user is not allowed to post
+			'core.posting_modify_template_vars'							=> 'on_displaying_posting_screen',				// Verify if the posting is allowed or not; display message if not
+			'core.posting_modify_submit_post_before'					=> 'on_submit_post',							// When user post a message, verify the introduce has been done
+			'core.submit_post_end'										=> 'on_submit_post_end',						// Change the url to go to if the user edit it's own unapproved introduction
+			'core.viewforum_get_topic_ids_data'							=> 'on_get_topic_ids',							// Allow the user that create own introduction to view it into the list of the topic, changing the SQL request to get approved topic + own introduce
+			'core.viewforum_modify_topicrow'							=> 'on_display_unapproved_question_mark',		// Allow displaying '?' into the topic list when the user see its own introduce
+			'core.phpbb_content_visibility_is_visible'					=> 'is_topic_visible',							// Allow the user that create own introduction to view it when it open the unapproved topic introduction. Else phpBB say that the topix doesn't exists.
+			'core.phpbb_content_visibility_get_visibility_sql_before'	=> 'get_topic_sql_visibility',					// Allow phpBB to retrieve a topic for the user that post it into introduce
+			'core.viewtopic_modify_post_row'							=> 'on_view_topic_modify_post_row_unapproved',	// Hide S_POST_UNAPPROVED if the user is into his own introduce (hide approved / unapproved) if has not this right.
+			
+/* Patch to add to posting.php :
+ * Search      : // Not able to reply to unapproved posts/topics
+ * Add-Before  :  
+// Feneck91 - Patch
+$vars = array(
+	'post_data',
+	'poll',
+	'mode',
+	'topic_id',
+	'forum_id',
+	'post_author_name',
+);
+extract($phpbb_dispatcher->trigger_event('core.posting_modify_databefore_patch', compact($vars)));
+*/
+			'core.posting_modify_databefore_patch'						=> 'on_user_want_post',							// Let the moderator to post into an unapproved post and user to edit own introduce.
 		);
 	}
 	
@@ -109,20 +133,168 @@ class introduciator_listener implements \Symfony\Component\EventDispatcher\Event
 	 * Verify if the posting is allowed or not.
 	 *
 	 * Called when the user submit a post.
+	 * If user is not allowed to post, an error message is displayed, else if 
+	 * the extension has been configure with force introduce approval, set option
+	 * to make this message with moderator approval.
 	 *
 	 * @param $event The event.
 	 */
 	public function on_submit_post($event)
 	{
-		if (!$this->introduciator_helper->introduciator_verify_posting($this->user, $event['mode'], $event['forum_id'], $event['post_id'], $event['post_data'], true))
-		{
-			$toto = 0;
+		if ($this->introduciator_helper->introduciator_verify_posting($this->user, $event['mode'], $event['forum_id'], $event['post_id'], $event['post_data'], true))
+		{	// Posting is allowed
+			$introduciator_posting_must_be_approved = $this->introduciator_helper->introduciator_is_posting_must_be_approved($this->user, $event['mode'], $event['data']['forum_id']);
+			if ($introduciator_posting_must_be_approved)
+			{	// If posting should not be approved, let $data['force_approved_state'] unchanged (in case of another MOD has modified it)
+				$data = $event['data'];
+				$data['force_visibility'] = ITEM_UNAPPROVED; // Force approval
+				$event['data'] = $data;
+			}
 		}
-		/*
-		$introduciator_posting_must_be_approved = introduciator_is_posting_must_be_approved($user, $mode, $forum_id);
-		if ($introduciator_posting_must_be_approved)
-		{	// If posting should not be approved, let $data['force_approved_state'] unchanged (in case of another MOD has modified it)
-			$data['force_approved_state'] = false; // Force approval
-		}*/
-	} 
+	}
+	
+
+	/**
+	 * Verify if the user is editing it's own introduction.
+	 *
+	 * Called when the user submit a post.
+	 * If user is not allowed to post, an error message is displayed, else if 
+	 * the extension has been configure with force introduce approval, set option
+	 * to make this message with moderator approval.
+	 *
+	 * @param $event The event.
+	 */
+	public function on_submit_post_end($event)
+	{
+		global $phpEx, $phpbb_root_path;
+		
+		switch ($event['mode'])
+		{
+			case 'edit_first_post':
+			case 'edit_last_post':
+			case 'edit':
+				if ($this->introduciator_helper->introduciator_is_topic_in_forum_is_unapproved_for_introduction($this->user, $event['data']['forum_id'], $event['data']['topic_id'], false))
+				{
+					$params = '&amp;t=' . $event['data']['topic_id'];
+					$params .= '&amp;p=' . $event['data']['post_id'];
+					$add_anchor = '#p' . $event['data']['post_id'];
+					$url = "{$phpbb_root_path}viewtopic.$phpEx";
+					$event['url'] = append_sid($url, 'f=' . $event['data']['forum_id'] . $params) . $add_anchor;
+				}
+			break;
+			case 'post':
+			case 'reply':
+			case 'quote':
+			case 'edit_topic':
+				// Nothing to do here
+			break;
+		}		
+	}
+	
+	/**
+	 * Allow the user that create own introduction to view it into the list of the topic, changing the SQL request to get approved topic + own introduce.
+	 *
+	 * @param $event The event.
+	*/
+	public function on_get_topic_ids($event)
+	{
+		if (!empty($event['sql_approved']))
+		{
+			$sql_approved = $this->introduciator_helper->introduciator_generate_sql_approved_for_forum($this->user, $event['forum_data']['forum_id'], $event['sql_approved'], 't');
+			if ($sql_approved !== $event['sql_approved'])
+			{	// Modified by function, should re-inject into sql statement
+				$sql_ary = $event['sql_ary'];
+				$sql_ary['WHERE'] = str_replace($event['sql_approved'], $sql_approved, $event['sql_ary']['WHERE']);
+				$event['sql_ary'] = $sql_ary;
+			}
+		}
+	}
+	
+	/**
+	 * Allow displaying '?' into the topic list when the user see its own introduce.
+	 *
+	 * Only in the INTRODUCIATOR_POSTING_APPROVAL_LEVEL_APPROVAL_WITH_EDIT mode.
+	 *
+	 * @param $event The event.
+	*/
+	public function on_display_unapproved_question_mark($event)
+	{
+		if ($this->introduciator_helper->introduciator_is_topic_in_forum_is_unapproved_for_introduction($this->user, $event['row']['forum_id'], $event['row']['topic_id'], false))
+		{
+			$topic_row = $event['topic_row'];
+			$topic_row['REPLIES'] = $topic_row['REPLIES'] + 1; // Else count = -1
+			$topic_row['S_TOPIC_UNAPPROVED'] = true;
+			$event['topic_row'] = $topic_row;
+		}
+	}
+	
+	/**
+	 * Allow the user that create own introduction to view it when it open the unapproved topic introduction.
+	 *
+	 * Else phpBB say that the topic doesn't exists.
+	 *
+	 * @param $event The event.
+	*/
+	public function is_topic_visible($event)
+	{
+		if ($event['mode'] === "topic")
+		{
+			if ($this->introduciator_helper->introduciator_is_topic_in_forum_is_unapproved_for_introduction($this->user, $event['forum_id'], $event['data']['topic_id'], false))
+			{
+				$event['is_visible'] = true;
+			}
+		}
+	}
+	
+	/**
+	 * Allow the user that create own introduction to view it into the list of the topic, even the topic is unapproved.
+	 *
+	 * @param $event The event.
+	*/
+	public function get_topic_sql_visibility($event)
+	{
+		$get_visibility_sql_overwrite = null;
+		if ($this->introduciator_helper->get_topic_sql_visibility($this->user, $event['forum_id'], $event['where_sql'], $event['mode'], $event['table_alias'], $get_visibility_sql_overwrite))
+		{
+			$event['get_visibility_sql_overwrite'] = $get_visibility_sql_overwrite;
+		}
+	}
+
+	/**
+	 * Called when the topic is view.
+	 * 
+	 * But it display Approve / Unapproved field even the user has no right to do this.
+	 * If it is a simple user with extension configured as "approvel with edit" we must
+	 * hide this fields.
+	 * Hide S_POST_UNAPPROVED if the user is into his own introduce.
+	 *
+	 * @param $event The event.
+	*/
+	public function on_view_topic_modify_post_row_unapproved($event)
+	{
+		if ($this->introduciator_helper->introduciator_is_topic_in_forum_is_unapproved_for_introduction($this->user, $event['topic_data']['forum_id'], $event['row']['topic_id'], false))
+		{
+			$row = $event['post_row'];
+			$row['S_POST_UNAPPROVED'] = false;
+			$event['post_row'] = $row;
+		}
+	}
+
+	/**
+	 * Called when a user whant to post, before write the message.
+	 * 
+	 * Only in the INTRODUCIATOR_POSTING_APPROVAL_LEVEL_APPROVAL_WITH_EDIT mode, allow the moderator to post a reply into an unapproved message.
+	 *
+	 * @param $event The event.
+	*/
+	public function on_user_want_post($event)
+	{
+		if ($this->introduciator_helper->introduciator_let_user_posting_or_editing($this->user, $event['mode'], $event['forum_id'], $event['topic_id'], $event['post_data']))
+		{
+			$data = $event['post_data'];
+			$data['topic_visibility'] = ITEM_APPROVED; // Force approval
+			$event['post_data'] = $data;
+		}
+	}
+
 }
