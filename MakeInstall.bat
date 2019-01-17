06cb271ae55b4b1bc3b75b3d9c34157f0ef69794
Feneck91 <schateau@laposte.net>
2019-01-17 22:25:36 +0100

    Fix MakeInstall batch to work like svn export but for git.

--- MakeInstall.bat
+++ MakeInstall.bat
@@ -1,6 +1,6 @@
 ECHO OFF
 
-SET INTRODUCIATOR_VERSION="2_1_0"
+SET INTRODUCIATOR_VERSION=2_1_0
 SET BATCH_PATH=%~dp0
 
 ECHO ------------------------------------
@@ -8,12 +8,6 @@ ECHO Creating Introduciator install kit
 ECHO Version :  %INTRODUCIATOR_VERSION%
 ECHO ------------------------------------
 ECHO.
-
-REM Delete the Introduciator export to make clean export
-if exist "%BATCH_PATH%Introduciator_v%INTRODUCIATOR_VERSION%" (
-    ECHO Deleting export folder...
-    RD "%BATCH_PATH%Introduciator_v%INTRODUCIATOR_VERSION%" /S /Q
-)
 
 REM Delete the Introduciator zip file to make clean export
 if exist "%BATCH_PATH%Introduciator_v%INTRODUCIATOR_VERSION%.zip" (
@@ -26,10 +20,11 @@ ECHO.
 ECHO ------------------------------------
 ECHO Exporting "Introduciator_v%INTRODUCIATOR_VERSION%"
 ECHO ------------------------------------
-REM svn export "https://subversion.assembla.com/svn/introduciator/trunk/Mod/feneck91" "%BATCH_PATH%Introduciator_v%INTRODUCIATOR_VERSION%"
-mkdir ""%BATCH_PATH%Introduciator_v%INTRODUCIATOR_VERSION%"
-svn export "%BATCH_PATH%Ext\feneck91" "%BATCH_PATH%Introduciator_v%INTRODUCIATOR_VERSION%\feneck91"
-IF %errorlevel% EQU 0 GOTO MakeZip
+pushd %CD%
+CD /D "%BATCH_PATH%Ext"
+git archive --format zip --output "%BATCH_PATH%Introduciator_v%INTRODUCIATOR_VERSION%.zip" "master"
+popd
+IF %errorlevel% EQU 0 GOTO Done
 
 REM Error while exporting Introduciator
 ECHO.
@@ -40,42 +35,10 @@ ECHO.
 pause
 GOTO End
 
-
-REM Make Zip file
-:MakeZip
-ECHO.
-ECHO ------------------------------------
-ECHO Creating Introduciator_v%INTRODUCIATOR_VERSION%.zip
-ECHO ------------------------------------
-PUSHD %CD%
-CD /D "%BATCH_PATH%\Introduciator_v%INTRODUCIATOR_VERSION%"
-"%BATCH_PATH%bin\zip.exe" -r "%BATCH_PATH%Introduciator_v%INTRODUCIATOR_VERSION%.zip" "feneck91"
-POPD
-IF %errorlevel% EQU 0 GOTO RemoveTempFolder
-GOTO Error
-
-:RemoveTempFolder
-REM Delete the Introduciator export to make clean export
-if exist "%BATCH_PATH%Introduciator_v%INTRODUCIATOR_VERSION%" (
-    ECHO Deleting export folder...
-    RD "%BATCH_PATH%Introduciator_v%INTRODUCIATOR_VERSION%" /S /Q
-)
-
-GOTO Done
-
-:Error
-REM Error while creating zip
-ECHO.
-ECHO.
-ECHO ------------------------------------
-ECHO Error while creating zip file!
-ECHO.
-pause
-GOTO End
-
 :Done
 ECHO.
-ECHO.
+ECHO.	
 ECHO "Introduciator_v%INTRODUCIATOR_VERSION%.zip" successfully created.
+pause
 
 :End
